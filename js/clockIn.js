ac435efba017bab2e9e326c26dd9413956dcb1a8
mushu1230 <62530889+mushu1230@users.noreply.github.com>
2020-04-26 09:30:14 +0800

    Update clockIn.js

--- js/clockIn.js
+++ js/clockIn.js
@@ -8,7 +8,7 @@ var map = new AMap.Map('container', {
 AMap.plugin('AMap.Geolocation', function() {
 	var geolocation = new AMap.Geolocation({
 		enableHighAccuracy: true, //是否使用高精度定位，默认:true
-		timeout: 2000, //超过10秒后停止定位，默认：5s
+		timeout: 500, //超过10秒后停止定位，默认：5s
 		buttonPosition: 'RB', //定位按钮的停靠位置
 		buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
 		zoomToAccuracy: true, //定位成功后是否自动调整地图视野到定位点

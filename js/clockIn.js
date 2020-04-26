b628f33ac7086892bf7e9693a778e9ccd10230ae
mushu1230 <62530889+mushu1230@users.noreply.github.com>
2020-04-26 09:03:59 +0800

    Update clockIn.js

--- js/clockIn.js
+++ js/clockIn.js
@@ -13,8 +13,6 @@ AMap.plugin('AMap.Geolocation', function() {
 		buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
 		zoomToAccuracy: true, //定位成功后是否自动调整地图视野到定位点
 		showCircle: false,
-		noIpLocate:3,
-		noGeLocation:3,
 	});
 	map.addControl(geolocation);
 	geolocation.getCurrentPosition(function(status, result) {
